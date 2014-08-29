34e58594278c01d25f7ac89ce156df0fd449c7ca
Doron Segal <doron2402@gmail.com>
2014-08-29 15:41:29 -0700

    update'

--- app/scripts/controllers/contact.js
+++ app/scripts/controllers/contact.js
@@ -8,9 +8,21 @@
  * Controller of the leads1App
  */
 angular.module('leads1App')
-  .controller('ContactCtrl', function ($scope) {
+  .controller('ContactCtrl', function ($scope, $http) {
       $scope.submitContactForm = function(contactInfo){
-        console.log(contactInfo);
+        if (!contactInfo || !contactInfo.email || !contactInfo.fname) {
+            window.alert('Give us something to work with...');
+        } else {
+            $http({method: 'POST', url: '/api/site/contactus', data: contactInfo}).
+                success(function(data, status, headers, config) {
+                  console.log(data);
+                }).
+                error(function(data, status, headers, config) {
+                    console.log(data);
+                    console.log(status);
+                });
+            console.log(contactInfo);
+        }
       };
       $scope.reset = function(){
         $scope.user = {};
